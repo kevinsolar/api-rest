a43b56f33f7a6fa1b1c16586c4b6e25647e1ecf6
Kevin BTA <kevinleitesolar@hotmail.com>
2025-07-07 17:25:41 -0300

    update: introduces the "user_id" from middleware to our POST.

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.get("/products", (req, res) => {
 app.post("/products", myMiddleware, (req, res) => {
 	const { name, price } = req.body
 
-	res.status(201).json({ name, price })
+	res.status(201).json({ name, price, user_id: req.user_id })
 })
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
