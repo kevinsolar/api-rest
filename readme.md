a5cdbd201467abb5af6d6c73a5d7e807bc4d3efc
Kevin BTA <kevinleitesolar@hotmail.com>
2025-07-07 17:23:30 -0300

    Initial commit. The first steps with the express framework for node api's

---
+++ readme.md
@@ -0,0 +1,3 @@
+### Usando tipagem Global em /types
+Criamos o arquivo .d.ts do TypeScript, para tipagem global, ode iremos declarar os valores que queremos utilizar.
+Com isso aprendi que posso usar de forma combinada ou sobreescrever as interfaces que tenho dentro, por exemplo, do Express
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express"
+import { myMiddleware } from "./middlewares/my-middleware"
+
+const PORT = 3333
+const app = express()
+app.use(express.json())
+
+/*
+ * Utilizando um Middleware de forma global
+ * // app.use(myMiddleware)
+ */
+
+app.get("/products", (req, res) => {
+	const { page, limit } = req.query
+
+	res.send(`Página ${page} de ${limit}`)
+})
+
+/*
+ * Para utilizar um middleware de forma "local" ou em um só lugar:
+ */
+app.post("/products", myMiddleware, (req, res) => {
+	const { name, price } = req.body
+
+	res.status(201).json({ name, price })
+})
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
